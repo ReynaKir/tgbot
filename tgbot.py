4e7370ab43ca69811bbc6e1178a1b0f06cbf3ef9
ReynaKir <6kiryaaa@gmail.com>
2026-06-06 18:06:55 +0300

    переход на вебхук

--- tgbot.py
+++ tgbot.py
@@ -1,16 +1,30 @@
 import os
+from fastapi import FastAPI, Request
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+app = FastAPI()
+
+telegram_app = Application.builder().token(TOKEN).build()
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я финансовый бот.")
+    await update.message.reply_text("Привет, чмоня! Я твой новый телеграм-бот.")
 
-app = Application.builder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+telegram_app.add_handler(CommandHandler("start", start))
 
-print("Бот запущен...")
 
-app.run_polling()
+@app.on_event("startup")
+async def on_startup():
+    await telegram_app.initialize()
+
+
+@app.post("/")
+async def webhook(request: Request):
+    data = await request.json()
+    update = Update.de_json(data, telegram_app.bot)
+    await telegram_app.process_update(update)
+    return {"ok": True}
